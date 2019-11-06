5e51a34b14b3f11dbd494b02d95a1e31a53a1ff5
sami <ydjm1027@naver.com>
2019-11-06 23:47:33 +0900

    map에 store marker + name 표시

--- server.js
+++ server.js
@@ -7,25 +7,28 @@ var connection = require('./mysql.js'); // mysql 연동
 
 
 // view 엔진 사용
+/*
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
+app.get('/', function (req, res) {
+	res.render('index');
+});
+
+*/
 
 app.use(express.static(path.join(__dirname, '/')));
 app.use(bodyParser());
 
-app.get('/', function (req, res) {
-	res.render('index');
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/www/index.html');
 });
 
 // mysql
 connection.connect();
 
-
-
 app.post('/map_post', function(req, res){
 	var x_lat = req.body.x_lat;
 	var y_lng = req.body.y_lng;
-	
 	
 	// 반경 500m
 	var sql = 'SELECT *,(6371*acos(cos(radians(?))*cos(radians(x_lat))*cos(radians(y_lng)-radians(?))+sin(radians(?))*sin(radians(x_lat)))) AS distance FROM test_table HAVING distance <= 0.5 ORDER BY distance LIMIT 0,300;'
